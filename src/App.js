92ba45dca831211007397ee3c9c1c17f668e21ad
priyankang <priyankak18@navgurukul.org>
2021-07-23 16:31:15 +0530

    cleaned the files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-// import emogiList from './emogiList.json'
 
-// import { Greek} from './components/Greek';
-// import Greek from './components/Greek';
+import './App.css';
 import SearchBar from './components/SearchBar';
-// import Welcome from './components/Welcome';
-// import Massage from './components/Massage';
-// import Card from "./components/Card";
-// import Counter from './components/Counter';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter /> */}
-      {/* <Massage /> */}
-      {/* <Greek /> */}
-      <SearchBar 
-      
-      />
-      
-       {/* <Card
-          name='Navgurukul' description='Ngo' age='04' img='https://content.jdmagicbox.com/comp/bangalore/w8/080pxx80.xx80.181203163638.t3w8/catalogue/navgurukul-bangalore-campus-huskur-bangalore-computer-training-insitutes-for-software-diploma-y2csavxaoh.jpg?clr=29333d' />
-        
-        <Card
-          name='Komal Bhatt' description='Team member' age='23' img='https://navgurukul.org/assets/img/main/komal.jpeg' />
-
-        <Card
-          name='Ravina Kumari' description='Alumina' age='21' img='https://navgurukul.org/assets/img/alumni/Ravina.jpeg' />
-
-        <Card
-          name='Rinki Kumari' description='Student' age='20' img='https://miro.medium.com/fit/c/131/131/2*1r58hhUoCL2QGcJ5UqIC-A.png' > </Card>
-              */}
+      <SearchBar />
     </div>
   );
 }
